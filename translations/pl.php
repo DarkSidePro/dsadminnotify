542c1267c1dd01d84b77f94a4b67d54d5adac1db
Patryk Tchorzewski <patryk@dark-side.pro>
2021-04-07 12:28:50 +0200

    correct license

--- translations/pl.php
+++ translations/pl.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <DARK SIDE TEAM> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ */
 global $_MODULE;
 $_MODULE = array();
 $_MODULE['<{adminnotify}prestashop>adminnotify_d3cfc87191e2f833900f90a9a3bf21f4'] = 'Admin Customer Control';
